d2bae208d5323c240f1c3c12b5066199250d5e15
igorkruf <igor.kruf@gmail.com>
2022-12-22 22:04:19 +0500

    first commit

--- index.js
+++ index.js
@@ -16,40 +16,45 @@ let downloadBicList = async (pathToBicList) => {
   let zipEntry = zip.getEntries();
   let nameXmlFile = zipEntry[0].entryName;
   zip.extractAllTo("./temp_xml/", true);
+  // Convert encoding streaming example
+  let fd1 = await fs.open(`./temp_xml/${nameXmlFile}`);
 
-  try {
-    let data = await fs.readFile(`./temp_xml/${nameXmlFile}`);
-    parseString(data, { trim: true }, function (err, result) {
-      let fullListBicDirectoryEntry = result.ED807.BICDirectoryEntry;
-      let clearListBicDirectoryEntry = fullListBicDirectoryEntry.filter(
-        (elem) => {
-          if (Object.keys(elem).includes("Accounts")) {
-            return true;
-          } else {
-            return false;
+  fd1
+    .createReadStream()
+    .pipe(iconv.decodeStream("Windows-1251"))
+    .pipe(iconv.encodeStream("utf-8"))
+    .pipe(createWriteStream(`./temp_xml/utf8_${nameXmlFile}`))
+    .on("finish", async () => {
+      let data = await fs.readFile(`./temp_xml/utf8_${nameXmlFile}`);
+      parseString(data, { trim: true }, function (err, result) {
+        let fullListBicDirectoryEntry = result.ED807.BICDirectoryEntry;
+        let clearListBicDirectoryEntry = fullListBicDirectoryEntry.filter(
+          (elem) => {
+            if (Object.keys(elem).includes("Accounts")) {
+              return true;
+            } else {
+              return false;
+            }
+          }
+        );
+        let finalListItem = {};
+        for (let i = 0; i < clearListBicDirectoryEntry.length; i++) {
+          for (
+            let y = 0;
+            y < clearListBicDirectoryEntry[i].Accounts.length;
+            y++
+          ) {
+            finalListItem.bic = clearListBicDirectoryEntry[i].$.BIC;
+            finalListItem.name =
+              clearListBicDirectoryEntry[i].ParticipantInfo[0].$.NameP;
+            finalListItem.corrAccount =
+              clearListBicDirectoryEntry[i].Accounts[y].$.Account;
+            finalList.push(finalListItem);
           }
         }
-      );
-      let finalListItem = {};
-      for (let i = 0; i < clearListBicDirectoryEntry.length; i++) {
-        for (
-          let y = 0;
-          y < clearListBicDirectoryEntry[i].Accounts.length;
-          y++
-        ) {
-          finalListItem.bic = clearListBicDirectoryEntry[i].$.BIC;
-          finalListItem.name =
-            clearListBicDirectoryEntry[i].ParticipantInfo[0].$.NameP;
-          finalListItem.corrAccount =
-            clearListBicDirectoryEntry[i].Accounts[y].$.Account;
-          finalList.push(finalListItem);
-        }
-      }
+      });
+      console.log(finalList);
     });
-  } catch (err) {
-    console.error(err);
-  }
-
-  console.log(finalList); //Выводим в консоль результат
 };
-downloadBicList("http://www.cbr.ru/s/newbik");
+let finalList = downloadBicList("http://www.cbr.ru/s/newbik");
+export default finalList;
